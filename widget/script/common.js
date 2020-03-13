e5701f6cd2e64dd2d12c9df92395bc958597b963
j0o0oker <1716081017@qq.com>
2020-03-13 15:42:57 +0800

    re

--- widget/script/common.js
+++ widget/script/common.js
@@ -1,230 +1,141 @@
-
-(function(window){
-	// Vue.directive('focus', {
-	// 	inserted: function(el, binding) {
-	// 		el.focus();
-	// 	}
-	// });
-    /*------------------------------------公共方法----------------------------------------*/
+var j = {};
 
 
-    /**
-     * toast提示
-     *
-     * @param {string} msg 提示语
-     */
-	var c = {};
-    c.toast = function(msg) {
-        if (typeof api === 'undefined') {
-            console.warn(msg);
+// 打开窗口
+
+j.openWin = function (url, param) {
+    api.openWin({
+        name: 'page1',
+        url: url,
+        pageParam: param
+    });
+}
+j.openWinNav = function (url, param, title) {
+    var name = url.slice(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
+    api.openTabLayout({
+        name: name,
+        url: url,
+        title: title,
+        pageParam: param,
+        hideNavigationBar: false,
+        navigationBar: {
+            height: 45,                  //（可选项）导航栏高度。默认值45。数字类型
+            background: '#fff',             //（可选项）导航栏背景。支持颜色值和图片，默认值#fff，字符串类型
+            shadow: '#fff',                  //（可选项）导航栏底部阴影线颜色。默认值#ddd，字符串类型
+            color: '#000',
+            leftButtons: [{
+                iconPath: 'widget://image/post/black_back_arrow.png'
+            }],
+            rightButtons: [{
+                iconPath: 'widget://image/post/dh.png'
+            }]
+
+        }
+    });
+}
+
+
+
+
+//公共功能
+/*
+*   获取当前经纬度
+*   param: {
+        callback: function (lat, lon) {
+                    lat: '维度'， lon：'经度'
+                  }
+*   }
+*/
+j.getCurLocation = function (callback) {
+    var resultList = api.hasPermission({
+        list:['location']
+    });
+    if (resultList[0].granted === false) {
+        j.log('没有定位权限');
+    }
+    api.requestPermission({
+        list:['location'],
+        code:1
+    }, function(ret, err) {
+        if (err) {
+            j.toast('获取定位权限失败');
             return;
         }
-        if(!msg){
-            return;
-        }
-
-        api.toast({
-            msg: msg,
-            duration: 3000,
-            location: 'bottom'
+        var resultList = api.hasPermission({
+            list:['location']
         });
-	};
-	/**
-     * alert提示
-     *
-     * @param {string} msg 提示语
-     */
-    c.alert = function(msg) {
-        if (typeof api === 'undefined') {
-            console.warn(msg);
-            return;
+        if (resultList[0].granted === false) {
+            j.log('没有定位权限');
         }
-        if(!msg){
-            return;
+        api.getLocation(function(ret, err) {
+            if (err) {
+                j.toast('获取当前地理位置信息失败');
+                return;
+            }
+            if (ret && ret.status) {
+                //ret.timestamp
+                callback(ret.latitude, ret.longitude);
+            }
+        });
+    });
+}
+
+
+
+// UI组件
+/*
+*打印
+*/
+j.log = function (content) {
+    if (typeof content === 'object' || typeof content === Object) {
+        console.log(JSON.stringify(content));
+        return;
+    }
+    if (typeof content === Array || typeof content === 'array') {
+        console.log(JSON.stringify(content));
+        return;
+    }
+    console.log(content);
+}
+// 弹窗提示
+j.toast = function (msg) {
+    api.toast({
+        msg: msg,
+        duration: 2000,
+        location: 'bottom'
+    });
+}
+// 下拉刷新
+j.pullDownRefresh = function (cb) {
+    var param = {
+        loadingImg: 'widget://image/public/wangyuanjing.png',
+        bgColor: 'rgba(0, 0, 0, 0)',
+        textColor: '#fff',
+        textDown: '下拉刷新...',
+        textUp: '松开刷新...'
+    };
+    api.setRefreshHeaderInfo(param, function(ret, err) {
+            cb(ret, err);
+            api.showProgress({
+                title: '努力加载中...',
+                text: '先喝杯茶...',
+                modal: false
+            });
+            api.refreshHeaderLoadDone();
+            setTimeout(function () {
+                api.hideProgress();
+            }, 2000);
+    });
+}
+// 上拉加载
+j.scrolltobottom = function (cb) {
+    api.addEventListener({
+        name:'scrolltobottom',
+        extra:{
+            threshold:0            //设置距离底部多少距离时触发，默认值为0，数字类型
         }
-
-        api.alert({
-			msg: msg,
-		});
-	};
-	
-	c.isObject = function(obj){
-		return typeof obj === 'object' && obj.constructor === {}.constructor;
-	};
-
-    c.isFunction = function( obj ) {
-        // Support: Chrome <=57, Firefox <=52
-        // In some browsers, typeof returns "function" for HTML <object> elements
-        // (i.e., `typeof document.createElement( "object" ) === "function"`).
-        // We don't want to classify *any* DOM node as a function.
-        return typeof obj === "function" && typeof obj.nodeType !== "number";
-	};
-	
-	c.openWin = function(url,param,title,name) {//打开全屏窗口
-		var getWinName = function(url){
-			if(!url){
-				return;
-			}
-			var winname = url;
-			var idx = winname.lastIndexOf('.');
-			if(idx > 0){
-				winname = winname.substring(0,idx);
-			}
-			var slash = url.lastIndexOf('/');
-			if(slash >= 0){
-				winname = winname.substring(slash+1);
-			}
-			return winname;
-		};
-
-		var len = arguments.length;
-		if(len <= 0 || !url){
-			return;
-		}
-		if($c.isObject(param)){
-			if(title){
-				param.toptitle = title;
-			}
-			api.openWin({
-				name: name || getWinName(url),
-				url: url,
-				bgColor: '#fff',
-				allowEdit:true,
-				bounces: false,
-				rect: {
-					x: 0,
-					y: 0,
-				},
-				pageParam: param
-			});
-		}
-		else{
-			api.openWin({
-				name: name || getWinName(url),
-				url: url,
-				bgColor: '#fff',
-				bounces: false,
-				rect: {
-					x: 0,
-					y: 0,
-				}
-			});
-		}
-	};
-
-	c.openWinNav = function(url,param,title,name) {//打开导航条窗口
-		var getWinName = function(url){
-			if(!url){
-				return;
-			}
-			var winname = url;
-			var idx = winname.lastIndexOf('.');
-			if(idx > 0){
-				winname = winname.substring(0,idx);
-			}
-			var slash = url.lastIndexOf('/');
-			if(slash >= 0){
-				winname = winname.substring(slash+1);
-			}
-			return winname;
-		};
-
-		var len = arguments.length;
-		if(len <= 0 || !url){
-			return;
-		}
-		var httpReg = /^(http|https):\/\/.+/i;
-
-		if(httpReg.test(url)){
-			title = arguments[1];
-			api.openWin({
-				name: 'outerlink',
-				url: '../html/common/outerlink.html',
-				title: title,
-				bgColor: '#fff',
-				bounces: false,
-				rect: {
-					x: 0,
-					y: 0,
-				},
-				pageParam: {
-					url: url,
-					title: title
-				}
-			});
-		}
-		else{
-			if(typeof arguments[1] == "string" ){
-				api.openTabLayout({
-					name: arguments[2] || getWinName(url),
-					url: url,
-					title: arguments[1],
-					hideNavigationBar: false,
-					bgColor: '#fff',
-					allowEdit: true,
-					navigationBar: {//默认值 tabBar 是54  , navigationBar是45
-						background: '#fff',
-						shadow: '#eee',
-						color: '#000',
-						// hideBackButton:false,
-						leftButtons: [{
-							// text:''
-							iconPath: 'widget://image/common/back_nav2.png'
-						}]
-					}
-				});
-			}
-			else{
-				if(param.rightIcon){
-					api.openTabLayout({
-						name: name || getWinName(url),
-						url: url,
-						title: title,
-						hideNavigationBar: false,
-						bgColor: '#fff',
-						allowEdit: true,
-						navigationBar: {//默认值 tabBar 是54  , navigationBar是45
-							background: '#fff',
-							shadow: '#eee',
-							color: '#000',
-							// hideBackButton:false,
-							leftButtons: [{
-								// text:''
-								iconPath: 'widget://image/common/back_nav2.png'
-							}],
-							rightButtons:[{
-								iconPath: 'widget://'+param.rightIcon
-							}]
-						},
-						pageParam: param
-					});
-				}
-				else{
-					api.openTabLayout({
-						name: name || getWinName(url),
-						url: url,
-						title: title,
-						hideNavigationBar: false,
-						bgColor: '#fff',
-						allowEdit: true,
-						navigationBar: {//默认值 tabBar 是54  , navigationBar是45
-							background: '#fff',
-							shadow: '#eee',
-							color: '#000',
-							// hideBackButton:false,
-							leftButtons: [{
-								// text:''
-								iconPath: 'widget://image/common/back_nav2.png'
-							}]
-						},
-						pageParam: param
-					});
-				}
-			}
-		}
-	};
-
-    window.$c = c;
-})(window);
-
-
+    }, function(ret, err){
+        cb(ret, err);
+    });
+}
+$j = j;
